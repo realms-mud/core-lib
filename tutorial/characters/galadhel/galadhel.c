867d8d71e09f0a4fb33bda2f900e040743441dad
Allen B. Cummings <abc@maelstrom.cc>
2018-04-18 19:57:50 -0400

    Merged PR 225: Merge 342_galadhel to master
    Related work items: #342

--- tutorial/characters/galadhel/galadhel.c
+++ tutorial/characters/galadhel/galadhel.c
@@ -2,3 +2,4 @@
 // Copyright (c) 2018 - Allen Cummings, RealmsMUD, All rights reserved. See
 //                      the accompanying LICENSE file for details.
 //*****************************************************************************
+virtual inherit "/lib/realizations/npc.c";
