75a890af176dbd7a13f8b45d56a448b3585887ee
Allen B. Cummings <abc@maelstrom.cc>
2018-06-20 13:04:27 -0400

    Merged PR 240: Merge 365_add_money to master
    Related work items: #365

---
+++ items/money.c
@@ -0,0 +1,58 @@
+//*****************************************************************************
+// Copyright (c) 2018 - Allen Cummings, RealmsMUD, All rights reserved. See
+//                      the accompanying LICENSE file for details.
+//*****************************************************************************
+virtual inherit "/lib/items/item.c";
+
+/////////////////////////////////////////////////////////////////////////////
+private string determineShortDescription()
+{
+    string ret = "A coin";
+    int value = query("value");
+    if (value > 1)
+    {
+        ret = sprintf("A pile of %d coins", value);
+    }
+    return ret;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+public nomask mixed query(string element)
+{
+    mixed ret = 0;
+    switch(element)
+    {
+        case "name":
+        case "type":
+        {
+            ret = "money";
+            break;
+        }
+        case "aliases":
+        {
+            ret = ({ "money", "coin", "coins", "currency", "pile of money", "pile of coins" });
+            break;
+        }
+        case "short":
+        {
+            if (query("value") < 1)
+            {
+                set("value", 1);
+            }
+            ret = determineShortDescription();
+            break;
+        }
+        default:
+        {
+            ret = "item"::query(element);
+        }
+    }
+    return ret;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+public varargs int get()
+{
+    int ret = !query("ungettable");
+    return ret;
+}
