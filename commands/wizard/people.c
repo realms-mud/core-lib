b0495684d8040ce11b7a58aead4dac6a2113522c
Allen B. Cummings <abc@maelstrom.cc>
2018-07-23 13:13:37 -0400

    Merged PR 270: Merge 398_people_command to master
    Related work items: #398

--- commands/wizard/people.c
+++ commands/wizard/people.c
@@ -4,14 +4,100 @@
 //*****************************************************************************
 inherit "/lib/commands/baseCommand.c";
 
+object Configuration;
+
 /////////////////////////////////////////////////////////////////////////////
 public nomask void reset(int arg)
 {
     if (!arg)
     {
         CommandType = "Wizard";
-        addCommandTemplate("people");
+        addCommandTemplate("people [-w] [-p]");
+        Configuration = load_object("/lib/dictionaries/configurationDictionary.c");
     }
+}
+
+/////////////////////////////////////////////////////////////////////////////
+private string getPlayerStatus(object player)
+{
+    string playerStatus = " ";
+    if (query_idle(player) > 300)
+    {
+        playerStatus = "I";
+    }
+    if ((member(inherit_list(player), "lib/realizations/wizard.c") > -1) &&
+        player->wizardStatus())
+    {
+        playerStatus = player->wizardStatus();
+    }
+    return playerStatus[0..0];
+}
+
+/////////////////////////////////////////////////////////////////////////////
+private string getPlayerLevel(object player)
+{
+    string level = to_string(player->effectiveLevel());
+    if (member(inherit_list(player), "lib/realizations/wizard.c") > -1)
+    {
+        level = capitalize(player->wizardLevel());
+    }
+    return sprintf("%-10s", level);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+private string getPlayerInfo(object player, object initiator)
+{
+    string colorConfiguration = initiator->colorConfiguration();
+
+    string address = Configuration->decorate(
+        sprintf("%-16s", query_ip_number(player) || "<unknown>"),
+        "ip address", "people", colorConfiguration);
+
+    string name = Configuration->decorate(
+        sprintf("%-20s", capitalize(player->RealName())),
+        "name", "people", colorConfiguration);
+
+    string level = Configuration->decorate(getPlayerLevel(player),
+        "level", "people", colorConfiguration);
+
+    string playerStatus = Configuration->decorate(getPlayerStatus(player),
+        "status", "people", colorConfiguration);
+
+    string location = Configuration->decorate((environment(player) ?
+        object_name(environment(player)) : "<unknown>"),
+        "location", "people", colorConfiguration);
+    location = regreplace(location, "players/", "~");
+
+    return address + name + level + playerStatus + "  " + location + "\n";
+}
+
+/////////////////////////////////////////////////////////////////////////////
+private object *getListOfUsers(string command)
+{
+    object *playerList = sort_array(users(),
+        (: $1->effectiveLevel() < $2->effectiveLevel() :));
+
+    object wizardDictionary = load_object("/lib/dictionaries/wizardDictionary.c");
+
+    object *wizardList = sort_array(filter(playerList,
+        (: (member(inherit_list($1), "lib/realizations/wizard.c") > -1) :)),
+        (: $3->getSortOrder($1) < $3->getSortOrder($2) :), wizardDictionary);
+
+    if (sizeof(regexp(({ command }), "-w")))
+    {
+        playerList = ({});
+    }
+    else
+    {
+        playerList -= wizardList;
+    }
+
+    if (sizeof(regexp(({ command }), "-p")))
+    {
+        wizardList = ({});
+    }
+
+    return wizardList + playerList;
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -22,6 +108,22 @@ public nomask int execute(string command, object initiator)
     if (canExecuteCommand(command) && initiator->hasExecuteAccess("people"))
     {
         ret = 1;
+
+        object *peopleList = getListOfUsers(command);
+
+        string details = sprintf("There are %d players (%d active)\n"
+            "%-16s%-20s%-10s   %s\n", sizeof(peopleList),
+            sizeof(filter(peopleList, (: query_idle($1) < 300 :))),
+            "IP Address", "Player Name", "Level", "Location");
+
+        foreach(object player in peopleList)
+        {
+            if (objectp(player))
+            {
+                details += getPlayerInfo(player, initiator);
+            }
+        }
+        tell_object(initiator, details);
     }
     return ret;
 }
---
+++ tests/commands/wizard/peopleTest.c
@@ -0,0 +1,137 @@
+//*****************************************************************************
+// Copyright (c) 2018 - Allen Cummings, RealmsMUD, All rights reserved. See
+//                      the accompanying LICENSE file for details.
+//*****************************************************************************
+inherit "/lib/tests/framework/testFixture.c";
+
+object Wizard;
+object Wizard2;
+object *Players;
+object Catch;
+
+/////////////////////////////////////////////////////////////////////////////
+int AdvanceToLevel(object player, int level, string guild)
+{
+    int runningLevel = player->guildLevel(guild);
+    while ((player->guildLevel(guild) < level) && player->memberOfGuild(guild))
+    {
+        player->addExperience(1000 * runningLevel);
+        player->advanceLevel(guild);
+        runningLevel = player->guildLevel(guild);
+    }
+    return runningLevel;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+void Init()
+{
+    ignoreList += ({ "AdvanceToLevel" });
+
+    setRestoreCaller(this_object());
+    object database = clone_object("/lib/tests/modules/secure/fakeDatabase.c");
+    database->PrepDatabase();
+
+    object dataAccess = clone_object("/lib/modules/secure/dataAccess.c");
+    dataAccess->savePlayerData(database->GetWizardOfLevel("elder"));
+
+    Wizard2 = clone_object("/lib/realizations/wizard.c");
+    Wizard2->restore("earl");
+    Wizard2->Title("the Doppleganger Not Really Named Earl");
+    Wizard2->addCommands();
+
+    dataAccess->savePlayerData(database->GetWizardOfLevel("admin"));
+
+    destruct(dataAccess);
+    destruct(database);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+void Setup()
+{
+    Catch = clone_object("/lib/tests/support/services/catchShadow.c");
+
+    Wizard = clone_object("/lib/realizations/wizard.c");
+    Wizard->restore("earl");
+    Wizard->addCommands();
+    Catch->beginShadow(Wizard);
+
+    Players = ({});
+
+    load_object("/lib/tests/support/guilds/testGuild.c")->PrepFakeGuild();
+
+    int level = 1;
+    foreach(string name in({ "fred", "george", "henrietta", "gertrude" }))
+    {
+        object player = clone_object("/lib/tests/support/services/mockPlayer.c");
+        player->Name(name);
+        player->joinGuild("test");
+        AdvanceToLevel(player, level, "test");
+        level++;
+        Players += ({ player });
+    }
+    Players[0]->Race("elf");
+    setUsers(Players + ({ Wizard, Wizard2 }));
+}
+
+/////////////////////////////////////////////////////////////////////////////
+void CleanUp()
+{
+    destruct(Wizard);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+void ExecuteRegexpIsNotGreedy()
+{
+    ExpectFalse(Wizard->executeCommand("wpeople"), "wpeople");
+    ExpectFalse(Wizard->executeCommand("peopleo"), "peopleo");
+}
+
+/////////////////////////////////////////////////////////////////////////////
+void PeopleDisplaysPeopleListInCorrectOrder()
+{
+    ExpectTrue(Wizard->executeCommand("people"));
+
+    string *peopleList = explode(Wizard->caughtMessage(), "\n");
+    ExpectEq("There are 6 players (6 active)", peopleList[0]);
+    ExpectEq("IP Address      Player Name         Level        Location", peopleList[1]);
+    ExpectSubStringMatch("Earl.*Elder", peopleList[2]);
+    ExpectSubStringMatch("Earl.*Admin", peopleList[3]);
+    ExpectSubStringMatch("Gertrude.*4", peopleList[4]);
+    ExpectSubStringMatch("Henrietta.*3", peopleList[5]);
+    ExpectSubStringMatch("George.*2", peopleList[6]);
+    ExpectSubStringMatch("Fred.*1", peopleList[7]);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+void PeopleWithWizardFilterDisplaysCorrectList()
+{
+    ExpectTrue(Wizard->executeCommand("people -w"));
+
+    string *peopleList = explode(Wizard->caughtMessage(), "\n");
+    ExpectEq(5, sizeof(peopleList));
+    ExpectSubStringMatch("Earl.*Elder", peopleList[2]);
+    ExpectSubStringMatch("Earl.*Admin", peopleList[3]);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+void PeopleWithPlayerFilterDisplaysCorrectList()
+{
+    ExpectTrue(Wizard->executeCommand("people -p"));
+
+    string *peopleList = explode(Wizard->caughtMessage(), "\n");
+    ExpectEq(7, sizeof(peopleList));
+    ExpectSubStringMatch("Gertrude.*4", peopleList[2]);
+    ExpectSubStringMatch("Henrietta.*3", peopleList[3]);
+    ExpectSubStringMatch("George.*2", peopleList[4]);
+    ExpectSubStringMatch("Fred.*1", peopleList[5]);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+void PeopleDisplaysCorrectLocation()
+{
+    move_object(Wizard, "/lib/environment/environment.c");
+    ExpectTrue(Wizard->executeCommand("people"));
+
+    string *peopleList = explode(Wizard->caughtMessage(), "\n");
+    ExpectSubStringMatch("Earl.*Admin.*lib/environment/environment", peopleList[3]);
+}
