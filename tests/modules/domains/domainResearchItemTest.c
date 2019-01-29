57c8b9ba6cdeca35eb770ab670ef3fb873d75e4b
Allen B. Cummings <abc@maelstrom.cc>
2019-01-29 16:22:31 -0500

    Merged PR 422: Merge 541_domain_research_trees to master
    Related work items: #541

---
+++ tests/modules/domains/domainResearchItemTest.c
@@ -0,0 +1,37 @@
+//*****************************************************************************
+// Copyright (c) 2019 - Allen Cummings, RealmsMUD, All rights reserved. See
+//                      the accompanying LICENSE file for details.
+//*****************************************************************************
+inherit "/lib/tests/framework/testFixture.c";
+
+object ResearchItem;
+
+/////////////////////////////////////////////////////////////////////////////
+void Setup()
+{
+    ResearchItem = clone_object("/lib/modules/domains/domainResearchItem");
+}
+
+/////////////////////////////////////////////////////////////////////////////
+void CleanUp()
+{
+    destruct(ResearchItem);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+void TypeIsKnowledge()
+{
+    ExpectEq("domain", ResearchItem->query("type"), "query the research type");
+}
+
+/////////////////////////////////////////////////////////////////////////////
+void ScopeIsSelf()
+{
+    ExpectEq("domain", ResearchItem->query("scope"), "query the research scope");
+}
+
+/////////////////////////////////////////////////////////////////////////////
+void EffectIsStrategic()
+{
+    ExpectEq("strategic", ResearchItem->query("effect"), "query the research effect");
+}
