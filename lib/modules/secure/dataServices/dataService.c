d0bf4c3ad28c187abf78f26bd101aa58bc15b773
Allen B. Cummings <abc@maelstrom.cc>
2020-02-11 13:04:39 -0500

    000_log_db_error_instead_of_raise

--- lib/modules/secure/dataServices/dataService.c
+++ lib/modules/secure/dataServices/dataService.c
@@ -29,7 +29,7 @@ protected nomask int connect()
     }
     if (db_error(ret))
     {
-        raise_error(db_error(ret));
+        getDictionary("log")->log("/log/DBERROR", db_error(ret));
     }
 
     if (ret)
