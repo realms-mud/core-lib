e4f30d2ba17cdcf11ceb62dd5e4c98303c7115f3
Allen B. Cummings <abc@maelstrom.cc>
2021-01-15 19:18:32 -0500

    Merged PR 832: 762_aegis_mythic_abilities
    Related work items: #762

--- guilds/aegis-guard/sword-mythic.c
+++ guilds/aegis-guard/sword-mythic.c
@@ -7,67 +7,136 @@ inherit "/lib/modules/research/researchTree.c";
 /////////////////////////////////////////////////////////////////////////////
 private void FifteenthLevel()
 {
+    addResearchElement("guilds/aegis-guard/forms/sword-mythic/auth.c");
 
+    addChild("guilds/aegis-guard/forms/sword-mythic/auth.c",
+        "guilds/aegis-guard/forms/sword-mythic/mythic-root.c");
 }
 
 /////////////////////////////////////////////////////////////////////////////
-private void SixteenthLevel()
+private void TwentyFirstLevel()
 {
+    addResearchElement("guilds/aegis-guard/forms/sword-mythic/braig-celair.c");
 
-}
-
-/////////////////////////////////////////////////////////////////////////////
-private void SeventeenthLevel()
-{
-
-}
-
-/////////////////////////////////////////////////////////////////////////////
-private void NineteenthLevel()
-{
-
-}
-
-/////////////////////////////////////////////////////////////////////////////
-private void TwentiethLevel()
-{
-
+    addChild("guilds/aegis-guard/forms/sword-mythic/braig-celair.c",
+        "guilds/aegis-guard/forms/sword-mythic/mythic-root.c");
 }
 
 /////////////////////////////////////////////////////////////////////////////
 private void TwentyThirdLevel()
 {
+    addResearchElement("guilds/aegis-guard/forms/sword-mythic/tur-celair.c");
 
+    addChild("guilds/aegis-guard/forms/sword-mythic/tur-celair.c",
+        "guilds/aegis-guard/forms/sword-mythic/mythic-root.c");
 }
 
 /////////////////////////////////////////////////////////////////////////////
 private void TwentyFifthLevel()
 {
+    addResearchElement("guilds/aegis-guard/forms/sword-mythic/delu-anwar.c");
 
-}
-
-/////////////////////////////////////////////////////////////////////////////
-private void TwentySeventhLevel()
-{
-
-}
-
-/////////////////////////////////////////////////////////////////////////////
-private void TwentyNinthLevel()
-{
-
-}
-
-/////////////////////////////////////////////////////////////////////////////
-private void ThirtiethLevel()
-{
-
+    addChild("guilds/aegis-guard/forms/sword-mythic/delu-anwar.c",
+        "guilds/aegis-guard/forms/sword-mythic/mythic-root.c");
 }
 
 /////////////////////////////////////////////////////////////////////////////
 private void ThirtyFirstLevel()
 {
+    addResearchElement("guilds/aegis-guard/forms/sword-mythic/braig-edlothia.c");
 
+    addChild("guilds/aegis-guard/forms/sword-mythic/braig-edlothia.c",
+        "guilds/aegis-guard/forms/sword-mythic/braig-celair.c");
+}
+
+/////////////////////////////////////////////////////////////////////////////
+private void ThirtyThirdLevel()
+{
+    addResearchElement("guilds/aegis-guard/forms/sword-mythic/tur-edlothia.c");
+
+    addChild("guilds/aegis-guard/forms/sword-mythic/tur-edlothia.c",
+        "guilds/aegis-guard/forms/sword-mythic/tur-celair.c");
+}
+
+/////////////////////////////////////////////////////////////////////////////
+private void ThirtyFifthLevel()
+{
+    addResearchElement("guilds/aegis-guard/forms/sword-mythic/delu-celair.c");
+
+    addChild("guilds/aegis-guard/forms/sword-mythic/delu-celair.c",
+        "guilds/aegis-guard/forms/sword-mythic/delu-anwar.c");
+}
+
+/////////////////////////////////////////////////////////////////////////////
+private void FortyFirstLevel()
+{
+    addResearchElement("guilds/aegis-guard/forms/sword-mythic/braig-manadh.c");
+
+    addChild("guilds/aegis-guard/forms/sword-mythic/braig-manadh.c",
+        "guilds/aegis-guard/forms/sword-mythic/braig-edlothia.c");
+}
+
+/////////////////////////////////////////////////////////////////////////////
+private void FortyThirdLevel()
+{
+    addResearchElement("guilds/aegis-guard/forms/sword-mythic/tur-manadh.c");
+
+    addChild("guilds/aegis-guard/forms/sword-mythic/tur-manadh.c",
+        "guilds/aegis-guard/forms/sword-mythic/tur-edlothia.c");
+}
+
+/////////////////////////////////////////////////////////////////////////////
+private void FortyFifthLevel()
+{
+    addResearchElement("guilds/aegis-guard/forms/sword-mythic/delu-edlothia.c");
+
+    addChild("guilds/aegis-guard/forms/sword-mythic/delu-edlothia.c",
+        "guilds/aegis-guard/forms/sword-mythic/delu-celair.c");
+}
+
+/////////////////////////////////////////////////////////////////////////////
+private void FiftyFirstLevel()
+{
+    addResearchElement("guilds/aegis-guard/forms/sword-mythic/braig-dagnir.c");
+
+    addChild("guilds/aegis-guard/forms/sword-mythic/braig-dagnir.c",
+        "guilds/aegis-guard/forms/sword-mythic/braig-manadh.c");
+}
+
+/////////////////////////////////////////////////////////////////////////////
+private void FiftyThirdLevel()
+{
+    addResearchElement("guilds/aegis-guard/forms/sword-mythic/tur-dagnir.c");
+
+    addChild("guilds/aegis-guard/forms/sword-mythic/tur-dagnir.c",
+        "guilds/aegis-guard/forms/sword-mythic/tur-manadh.c");
+}
+
+/////////////////////////////////////////////////////////////////////////////
+private void FiftyFifthLevel()
+{
+    addResearchElement("guilds/aegis-guard/forms/sword-mythic/delu-eiliant.c");
+
+    addChild("guilds/aegis-guard/forms/sword-mythic/delu-eiliant.c",
+        "guilds/aegis-guard/forms/sword-mythic/delu-edlothia.c");
+}
+
+/////////////////////////////////////////////////////////////////////////////
+private void SixtyFifthLevel()
+{
+    addResearchElement("guilds/aegis-guard/forms/sword-mythic/delu-manadh.c");
+
+    addChild("guilds/aegis-guard/forms/sword-mythic/delu-manadh.c",
+        "guilds/aegis-guard/forms/sword-mythic/delu-eiliant.c");
+}
+
+/////////////////////////////////////////////////////////////////////////////
+private void SeventyFifthLevel()
+{
+    addResearchElement("guilds/aegis-guard/forms/sword-mythic/delu-dagnir.c");
+
+    addChild("guilds/aegis-guard/forms/sword-mythic/delu-dagnir.c",
+        "guilds/aegis-guard/forms/sword-mythic/delu-manadh.c");
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -76,7 +145,7 @@ public void reset(int arg)
     if (!arg)
     {
         researchTree::reset(arg);
-        Name("Manadh ai Megil");
+        Name("Arnen ai Megil");
         Description("");
         Source("Aegis Guard");
         addResearchElement(
@@ -85,15 +154,19 @@ public void reset(int arg)
             "guilds/aegis-guard/forms/sword-mythic/mythic-root.c");
 
         FifteenthLevel();
-        SixteenthLevel();
-        SeventeenthLevel();
-        NineteenthLevel();
-        TwentiethLevel();
+        TwentyFirstLevel();
         TwentyThirdLevel();
         TwentyFifthLevel();
-        TwentySeventhLevel();
-        TwentyNinthLevel();
-        ThirtiethLevel();
         ThirtyFirstLevel();
+        ThirtyThirdLevel();
+        ThirtyFifthLevel();
+        FortyFirstLevel();
+        FortyThirdLevel();
+        FortyFifthLevel();
+        FiftyFirstLevel();
+        FiftyThirdLevel();
+        FiftyFifthLevel();
+        SixtyFifthLevel();
+        SeventyFifthLevel();
     }
 }
