9a6b3453c4e3e0015a57b17227e722191c96a6f0
Allen B. Cummings <abc@maelstrom.cc>
2021-04-27 15:12:02 -0400

    Merged PR 928: 990_aeromancer
    Initial
    
    Related Work Items: #990
    
    Related work items: #990

---
+++ guilds/aeromancer/lightning/root.c
@@ -0,0 +1,19 @@
+//*****************************************************************************
+// Copyright (c) 2021 - Allen Cummings, RealmsMUD, All rights reserved. See
+//                      the accompanying LICENSE file for details.
+//*****************************************************************************
+inherit "/lib/modules/research/passiveResearchItem.c";
+
+/////////////////////////////////////////////////////////////////////////////
+protected void Setup()
+{
+    addSpecification("name", "Lightning Spells");
+    addSpecification("source", "aeromancer");
+    addSpecification("description", "This skill provides the user with the "
+        "knowledge of lightning-based spells for Aeromancers.");
+
+    addSpecification("scope", "self");
+    addSpecification("research type", "tree root");
+    addSpecification("bonus elemental air", 1);
+    addSpecification("bonus magical essence", 1);
+}
