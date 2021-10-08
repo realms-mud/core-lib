bbe9234d7b0716480d0b5cc47d4849492bcf5902
Allen B. Cummings <abc@maelstrom.cc>
2021-10-08 17:28:43 -0400

    Merged PR 981: 1091_commanders_for_units
    Related work items: #1091

--- lib/dictionaries/domains/sub-dictionaries/units.c
+++ lib/dictionaries/domains/sub-dictionaries/units.c
@@ -20,7 +20,34 @@ private nomask float getOwnerBonus(object unit, string type)
 public nomask float getDefenseBonus(object unit, object target)
 {
     return 1.0 + getCommanderBonus(unit, "defense") +
-        getOwnerBonus(unit, "defense") - 
+        getOwnerBonus(unit, "defense") -
         getCommanderBonus(target, "defense") -
         getOwnerBonus(target, "defense");
+}
+
+/////////////////////////////////////////////////////////////////////////////
+public nomask float getAttackBonus(object unit, object target)
+{
+    return 1.0 + getCommanderBonus(unit, "attack") +
+        getOwnerBonus(unit, "attack") -
+        getCommanderBonus(target, "attack") -
+        getOwnerBonus(target, "attack");
+}
+
+/////////////////////////////////////////////////////////////////////////////
+public nomask float getDamageBonus(object unit, object target)
+{
+    return 1.0 + getCommanderBonus(unit, "damage") +
+        getOwnerBonus(unit, "damage") -
+        getCommanderBonus(target, "damage") -
+        getOwnerBonus(target, "damage");
+}
+
+/////////////////////////////////////////////////////////////////////////////
+public nomask float getHealthBonus(object unit, object target)
+{
+    return 1.0 + getCommanderBonus(unit, "health") +
+        getOwnerBonus(unit, "health") -
+        getCommanderBonus(target, "health") -
+        getOwnerBonus(target, "health");
 }
