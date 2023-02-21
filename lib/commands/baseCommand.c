f7886d2221f18959b2a879e00f2bd5ce99a234c4
realms-mud <maeglin@maelstrom.cc>
2023-02-21 12:25:31 -0500

    000__year_end_bookkeeping

--- lib/commands/baseCommand.c
+++ lib/commands/baseCommand.c
@@ -2,7 +2,6 @@
 // Copyright (c) 2021 - Allen Cummings, RealmsMUD, All rights reserved. See
 //                      the accompanying LICENSE file for details.
 //*****************************************************************************
-
 private string MaterialAttributes = "/lib/modules/materialAttributes.c";
 private string MessageParser = "/lib/core/messageParser.c";
 
