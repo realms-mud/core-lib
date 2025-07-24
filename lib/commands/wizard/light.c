3bb1f859a3ad966d3e79210d6db52ff31fe9ffe4
Allen Cummings <abc@maelstrom.cc>
2025-07-24 22:05:36 +0100

    1103: light command

--- lib/commands/wizard/light.c
+++ lib/commands/wizard/light.c
@@ -4,11 +4,29 @@
 //*****************************************************************************
 inherit "/lib/commands/baseCommand.c";
 
+#define WIZARD_LIGHT_NAME "wizard-light"
+#define WIZARD_LIGHT_PATH "/lib/items/item.c"
+
 /////////////////////////////////////////////////////////////////////////////
 public nomask void SetupCommand()
 {
     CommandType = "Wizard";
-    addCommandTemplate("light [-l ##Level##]");
+    addCommandTemplate("light [-l ##Level##] [.*]");
+    addCommandTemplate("light off");
+}
+
+/////////////////////////////////////////////////////////////////////////////
+private object findWizardLight(object wizard)
+{
+    object *inv = all_inventory(wizard);
+    foreach(object item in inv)
+    {
+        if (item && item->id(WIZARD_LIGHT_NAME))
+        {
+            return item;
+        }
+    }
+    return 0;
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -16,9 +34,103 @@ public nomask int execute(string command, object initiator)
 {
     int ret = 0;
 
-    if (canExecuteCommand(command))
+    if (canExecuteCommand(command) && objectp(initiator))
     {
-        ret = 1;
+        if (command == "light off")
+        {
+            object light = findWizardLight(initiator);
+            if (light)
+            {
+                destruct(light);
+                tell_object(initiator, "Your wizard light is extinguished.\n");
+            }
+            else
+            {
+                tell_object(initiator, "You have no wizard light to extinguish.\n");
+            }
+            ret = 1;
+        }
+        else
+        {
+            int level = 10;
+            if (sizeof(regexp(({ command }), "-l *[0-9]+")))
+            {
+                string levelStr = regreplace(command, ".*-l *([0-9]+).*", "\\1", 1);
+                int parsed = to_int(levelStr);
+                if (parsed > 0)
+                {
+                    level = parsed;
+                }
+            }
+
+            object light = findWizardLight(initiator);
+            if (!light)
+            {
+                light = clone_object(WIZARD_LIGHT_PATH);
+                light->set("name", WIZARD_LIGHT_NAME);
+                light->set("aliases", ({ WIZARD_LIGHT_NAME }));
+                light->set("short", "");
+                light->set("long", "");
+                light->set("light", level);
+                move_object(light, initiator);
+                tell_object(initiator, sprintf("A wizard light (level %d) now illuminates you.\n", level));
+
+                ret = 1;
+            }
+            else
+            {
+                light->set("light", level);
+                tell_object(initiator, sprintf("Your wizard light is now set to level %d.\n", level));
+                ret = 1;
+            }
+        }
     }
     return ret;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+protected string wildcardMeaning(string colorConfiguration)
+{
+    return configuration->decorate("<File to view>",
+        "wildcard", "help", colorConfiguration);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+protected string synopsis(string displayCommand, string colorConfiguration)
+{
+    return format("Display the last N lines of a file.", 78);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+protected string flagInformation(string flag, string colorConfiguration)
+{
+    string ret = "";
+    string parsedFlag = regreplace(flag, "[^-]*(-[a-zA-Z]+).*", "\\1");
+    switch (parsedFlag)
+    {
+        case "-n":
+        {
+            ret = "Specifies the number of lines to display from the end of the file. "
+                  "If omitted, the last 8 lines are shown by default.";
+            break;
+        }
+    }
+    return format(ret, 72);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+protected string description(string displayCommand, string colorConfiguration)
+{
+    return format("The 'tail' command displays the last 8 lines of the "
+        "specified file, provided the wizard has read permission. "
+        "If the optional -n flag is used, it displays the last N lines instead. "
+        "This is useful for quickly viewing recent log entries or file changes.",
+        78);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+protected string notes(string displayCommand, string colorConfiguration)
+{
+    return format("See also: more, cat, head", 78);
 }
---
+++ lib/tests/commands/wizard/lightTest.c
@@ -0,0 +1,142 @@
+//*****************************************************************************
+// Copyright (c) 2025 - Allen Cummings, RealmsMUD, All rights reserved. See
+//*****************************************************************************
+inherit "/lib/tests/framework/testFixture.c";
+
+object Wizard;
+string LightName = "wizard-light";
+string LightPath = "/lib/items/item.c";
+
+///////////////////////////////////////////////////////////////////////////////
+void Init()
+{
+    ignoreList += ({ "getWizardLight" });
+}
+
+/////////////////////////////////////////////////////////////////////////////
+void Setup()
+{
+    Wizard = clone_object("/lib/tests/support/services/mockWizard.c");
+    Wizard.Name("bob");
+    Wizard.addCommands();
+    Wizard.colorConfiguration("none");
+
+    move_object(Wizard, "/lib/tests/support/environment/testEnvironment.c");
+}
+
+/////////////////////////////////////////////////////////////////////////////
+void CleanUp()
+{
+    destruct(Wizard);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+object getWizardLight()
+{
+    object *inv = all_inventory(Wizard);
+    foreach(object item in inv)
+    {
+        if (item && item->id(LightName))
+        {
+            return item;
+        }
+    }
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+void LightCommandCreatesLightAtDefaultLevel()
+{
+    ExpectTrue(Wizard.executeCommand("light"), "Executed light command");
+    object light = getWizardLight();
+    ExpectTrue(objectp(light), "Expected a light object to be created.");
+    ExpectEq(10, light->query("light"), "Expected light level to be 10.");
+    ExpectEq("", light->short(), "Expected light to have no short description.");
+    ExpectEq(LightName, light->query("name"), "Expected light name to match.");
+    ExpectSubStringMatch("illuminates you", implode(Wizard->caughtMessages(), "\n"));
+}
+
+/////////////////////////////////////////////////////////////////////////////
+void LightCommandCreatesLightAtCustomLevel()
+{
+    command("light -l 3", Wizard);
+
+    object light = getWizardLight();
+    ExpectTrue(objectp(light));
+    ExpectEq(3, light->query("light"));
+    ExpectSubStringMatch("level 3", Wizard->caughtMessage());
+}
+
+/////////////////////////////////////////////////////////////////////////////
+void LightCommandDoesNotCloneMultipleLights()
+{
+    ExpectTrue(Wizard.executeCommand("light -l 5"));
+    object light1 = getWizardLight();
+    ExpectTrue(objectp(light1));
+    ExpectEq(5, light1->query("light"));
+
+    ExpectTrue(Wizard.executeCommand("light -l 7"));
+    object light2 = getWizardLight();
+    ExpectTrue(objectp(light2));
+    ExpectEq(light1, light2);
+    ExpectEq(7, light2->query("light"));
+}
+
+/////////////////////////////////////////////////////////////////////////////
+void LightOffDestroysLight()
+{
+    ExpectTrue(Wizard.executeCommand("light"));
+    object light = getWizardLight();
+    ExpectTrue(objectp(light));
+
+    ExpectTrue(Wizard.executeCommand("light off"));
+    ExpectFalse(objectp(getWizardLight()));
+    ExpectSubStringMatch("extinguished", Wizard->caughtMessage());
+}
+
+/////////////////////////////////////////////////////////////////////////////
+void LightOffWithNoLightShowsMessage()
+{
+    ExpectFalse(objectp(getWizardLight()));
+    ExpectTrue(Wizard.executeCommand("light off"));
+    ExpectSubStringMatch("no wizard light", implode(Wizard->caughtMessages(), "\n"));
+}
+
+/////////////////////////////////////////////////////////////////////////////
+void LightIsInvisibleToLook()
+{
+    ExpectTrue(Wizard.executeCommand("light"));
+    object light = getWizardLight();
+    ExpectTrue(objectp(light));
+    ExpectEq("", light->short());
+}
+
+/////////////////////////////////////////////////////////////////////////////
+void LightOnlyAffectsWizard()
+{
+    object other = clone_object("/lib/tests/support/services/mockPlayer.c");
+    other.Name("alice");
+    move_object(other, environment(Wizard));
+    other.addCommands();
+
+    ExpectTrue(Wizard.executeCommand("light -l 15"));
+    object light = getWizardLight();
+    ExpectTrue(objectp(light));
+    ExpectEq(environment(light), Wizard);
+
+    // Simulate environment light check
+    int wizardLight = 0;
+    int otherLight = 0;
+    foreach(object item in all_inventory(Wizard))
+    {
+        wizardLight += item->query("light");
+    }
+    foreach(object item in all_inventory(other))
+    {
+        otherLight += item->query("light");
+    }
+    ExpectEq(15, wizardLight);
+    ExpectEq(0, otherLight);
+
+    destruct(other);
+}
