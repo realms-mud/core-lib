ff16439615d67d8ef495913a2571b480fc95afd0
Allen Cummings <abc@maelstrom.cc>
2026-02-27 19:03:55 +0000

    #983: Initial areas

--- areas/eledhel/southern-city/21x9.c
+++ areas/eledhel/southern-city/21x9.c
@@ -7,9 +7,8 @@ inherit "/lib/environment/environment.c";
 /////////////////////////////////////////////////////////////////////////////
 public void Setup()
 {
-    setInterior("/lib/environment/interiors/wood-interior-shop.c");
+    setInterior("/lib/environment/items/shops/grocer.c");
     addItem("/lib/environment/items/lighting/window.c", "west");
-    addItem("/lib/environment/items/shops/grocer.c");
 
     addDecorator("2x1 grocer 0x0");
     addExit("west", "/areas/eledhel/southern-city/20x9.c");
