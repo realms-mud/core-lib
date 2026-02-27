1a70489f027247c23e158606a2e1f45b02e6b4ca
Allen Cummings <abc@maelstrom.cc>
2026-02-27 18:54:04 +0000

    #983: Initial areas

--- lib/environment/items/shops/bakery.c
+++ lib/environment/items/shops/bakery.c
@@ -2,7 +2,7 @@
 // Copyright (c) 2017-2026 - Allen Cummings, RealmsMUD, All rights reserved. See
 //                      the accompanying LICENSE file for details.
 //*****************************************************************************
-inherit "/lib/environment/items/baseItem.c";
+inherit "/lib/environment/interiors/baseInterior.c";
 
 /////////////////////////////////////////////////////////////////////////////
 public void Setup()
